bbfaec58756d6df3fd1b6e603625c29348dbcafb
Dan Phan Manh <danpm@fpt.com>
2024-01-28 10:57:33 +0800

    Adds code to convert to Kindle and other dicts
    Former-commit-id: 713c3eeff52826944b49585ed92e02fe34a0e566 [formerly 8098211d0d6407fcb733cf58b94d79c83dc9fb48]
    Former-commit-id: ca186497b063c99da3061b0a471f849f82201171

---
+++ pleco_to_other_dicts.py
@@ -0,0 +1,34 @@
+import os
+
+dict_names = ["char_dict_pleco.txt", "radical_lookup_pleco.txt", "radical_name_pleco.txt", "tvb_pleco.txt"]
+
+replaces = {
+    chr(0xEAB1): "<br>",
+    chr(0xEAB2): "<b>",
+    chr(0xEAB3): "</b>",
+    chr(0xEAB4): "<i>",
+    chr(0xEAB5): "</i>",
+    chr(0xEAB8): "",
+    chr(0xEABB): "",
+    "": "",
+    "": "",
+    "": "",
+}
+
+for filename in dict_names:
+    with open(filename, "r", encoding="utf-8") as fread:
+        name, ext = os.path.splitext(filename)
+        contents = fread.read()
+
+        for key in replaces:
+            contents = contents.replace(key, replaces[key])
+
+        lines = contents.split("\n")
+
+        with open(name + ".tab", "w", encoding="utf-8") as fwrite:
+            for line in lines[1:]:
+                if not line.strip():
+                    continue
+
+                items = line.split("\t")
+                fwrite.write(f"{items[0]}\t{items[1]} {items[2]}\n")
