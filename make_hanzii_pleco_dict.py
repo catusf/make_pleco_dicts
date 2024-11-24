503b0ca3cee5545a5d4149e5b44a6b538d174a11
Catus Phan <catus.phan@gmail.com>
2024-11-24 17:16:16 +0800

    Fixes errors creating TV dicsts

--- make_hanzii_pleco_dict.py
+++ make_hanzii_pleco_dict.py
@@ -32,18 +32,27 @@ def main():
         required=False,
         help="Dictionary size: 'small' for Vietnamese definitions, 'mid' for definitions and examples, 'big' for all definitions including Chinese.",
     )
+    parser.add_argument(
+        "--num-items",
+        type=int,
+        default=100000000,
+        required=False,
+        help="Dictionary size: 'small' for Vietnamese definitions, 'mid' for definitions and examples, 'big' for all definitions including Chinese.",
+    )
     args = parser.parse_args()
 
     dict_size = args.dict_size
+    num_items = args.num_items
 
     now_datetime = datetime.datetime.now()
     now_str = now_datetime.strftime("%Y-%m-%d_%H-%M-%S")
 
     signal.signal(signal.SIGINT, keyboard_handler)
 
-    print("Open new recommendation file")
-    with open(join(DATA_DIR, "new_reccommendations.json"), "r", encoding="utf-8") as fread:
-        new_recomend = json.load(fread)
+    if dict_size in ["big"]:
+        print("Open new recommendation file")
+        with open(join(DATA_DIR, "new_reccommendations.json"), "r", encoding="utf-8") as fread:
+            new_recomend = json.load(fread)
 
     print("Open dictionary data file")
     with open(join(DATA_DIR, "dict_data.json"), "r", encoding="utf-8") as fread:
@@ -54,19 +63,24 @@ def main():
 
     PC_DICT_NAME = "//Trung-Viet Beta Dict"
     # MAX_ITEMS = 1000000
-    MAX_ITEMS = 100
+    # MAX_ITEMS = 100
+    REPORT_COUNT = 1000
 
-    with open(join(DICT_DIR, "tvb_pleco.txt"), "w", encoding="utf-8") as pleco_import_file:
+    output_file = join(DICT_DIR, f"TrungViet-{dict_size}_pleco.txt")
+    with open(output_file, "w", encoding="utf-8") as pleco_import_file:
         if MAKE_PLECO:
             pleco_import_file.write(f"{PC_DICT_NAME}\n")
 
         for num, headword in enumerate(sorted(dict_data)):
             dict_item = dict_data[headword]
 
-            if num >= MAX_ITEMS:
+            if num >= num_items:
                 break
-
-            print(f"Processing {num+1}: {headword}...")
+            
+            if (num + 1) % REPORT_COUNT == 0:
+                print(f"Processing item {num+1}...")
+                 
+            # print(f"Processing {num+1}: {headword}...")
             pleco_string = ""
 
             if headword not in current_word_list:
@@ -99,38 +113,42 @@ def main():
 
                     pleco_string += f"{item['definition']['vietnamese']}\n"
 
-                    example = item["definition"]["example"]
-
                     if dict_size in ["mid", "big"]:
-                        pleco_string += (
-                            f"{pleco_make_dark_gray(PC_DIAMOND + " " + PC_VIDU_NEW_MARK, make_pleco=MAKE_PLECO)}\n "
-                        )
-                        pleco_string += f"{pleco_make_blue(example['example_chinese'], make_pleco=MAKE_PLECO)} "
-                        pleco_string += f"{pleco_make_italic(example['example_pron'], make_pleco=MAKE_PLECO)} "
-                        pleco_string += f"{example['example_meaning']}\n"
+                        example = item["definition"]["example"]
 
-                    if reccs := new_recomend[headword] and dict_size in ["big"]:
-                        pleco_string += f"\n{pleco_make_dark_gray(
-                            PC_CLUB_SUIT, make_pleco=MAKE_PLECO)} {pleco_make_dark_gray(PC_RELATED_MARK, make_pleco=MAKE_PLECO)}\n"
+                        if example:
+                            pleco_string += (
+                                f"{pleco_make_dark_gray(PC_DIAMOND + " " + PC_VIDU_NEW_MARK, make_pleco=MAKE_PLECO)}\n "
+                            )
+                            pleco_string += f"{pleco_make_blue(example['example_chinese'], make_pleco=MAKE_PLECO)} "
+                            pleco_string += f"{pleco_make_italic(example['example_pron'], make_pleco=MAKE_PLECO)} "
+                            pleco_string += f"{example['example_meaning']}\n"
 
-                        for rec in reccs:
-                            key = list(rec.keys())[0]
+                    if dict_size in ["big"]:
+                        reccs = new_recomend[headword]
+                        if reccs:
+                            pleco_string += f"\n{pleco_make_dark_gray(
+                                PC_CLUB_SUIT, make_pleco=MAKE_PLECO)} {pleco_make_dark_gray(PC_RELATED_MARK, make_pleco=MAKE_PLECO)}\n"
 
-                            item = rec[key]
+                            for rec in reccs:
+                                key = list(rec.keys())[0]
 
-                            # print(f"{key} {item['mean']} {item['pinyin']}")
+                                item = rec[key]
 
-                            if key in dict_data:
-                                pleco_string += f"{pleco_make_dark_gray(PC_ARROW, make_pleco=MAKE_PLECO)} {pleco_make_link(key, make_pleco=MAKE_PLECO)} {
-                                    pleco_make_italic(item['pinyin'], make_pleco=MAKE_PLECO)} {item['mean']}\n"
-                            else:
-                                pleco_string += f"{pleco_make_dark_gray(PC_ARROW, make_pleco=MAKE_PLECO)} {pleco_make_blue(key, make_pleco=MAKE_PLECO)} {
-                                    pleco_make_italic(item['pinyin'])} {item['mean']}\n"
+                                # print(f"{key} {item['mean']} {item['pinyin']}")
+
+                                if key in dict_data:
+                                    pleco_string += f"{pleco_make_dark_gray(PC_ARROW, make_pleco=MAKE_PLECO)} {pleco_make_link(key, make_pleco=MAKE_PLECO)} {
+                                        pleco_make_italic(item['pinyin'], make_pleco=MAKE_PLECO)} {item['mean']}\n"
+                                else:
+                                    pleco_string += f"{pleco_make_dark_gray(PC_ARROW, make_pleco=MAKE_PLECO)} {pleco_make_blue(key, make_pleco=MAKE_PLECO)} {
+                                        pleco_make_italic(item['pinyin'])} {item['mean']}\n"
 
             pleco_string = pleco_string.replace("\n\n", "\n")
             pleco_string = pleco_make_new_line(pleco_string)
             pleco_import_file.write(f"{pleco_string}\n")
 
+    print(f"Written output to {output_file}")
     later_datetime = datetime.datetime.now()
     print(f"Time elapsed {later_datetime-now_datetime}")
 
